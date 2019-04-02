e85eef55e2ac3c922ef41b80e7a36c1a9201733c
ado999 <anonymous.zebro@gmail.com>
2019-04-02 18:35:02 +0200

    Initial commit

---
+++ src/main/java/pl/edu/wat/wcy/tim/blackduck/BlackduckApplication.java
@@ -0,0 +1,13 @@
+package pl.edu.wat.wcy.tim.blackduck;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BlackduckApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BlackduckApplication.class, args);
+    }
+
+}
