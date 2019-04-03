6bee78ab99121241b4cdabd910debb8608eaed67
ziooolo <a.ziolko96@wp.pl>
2019-04-04 00:17:19 +0200

    - dependencies for jwt - user - role in user model - WebSecurityConfig - security package and services for user implementation - login/signup request - jwt response

---
+++ src/main/java/pl/edu/wat/wcy/tim/blackduck/services/UserService.java
@@ -0,0 +1,8 @@
+package pl.edu.wat.wcy.tim.blackduck.services;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import pl.edu.wat.wcy.tim.blackduck.repositories.UserRepository;
+
+public class UserService {
+
+}
