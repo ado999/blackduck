588a09a471bcd01313cfa29b781a049692c0ed33
ado999 <anonymous.zebro@gmail.com>
2019-04-07 09:55:15 +0200

    added: messaging v2

--- src/main/java/pl/edu/wat/wcy/tim/blackduck/main/BlackduckApplication.java
+++ src/main/java/pl/edu/wat/wcy/tim/blackduck/main/BlackduckApplication.java
@@ -2,8 +2,10 @@ package pl.edu.wat.wcy.tim.blackduck.main;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
+@ComponentScan("pl.edu.wat.wcy.tim.blackduck")
 public class BlackduckApplication {
 
     public static void main(String[] args) {
